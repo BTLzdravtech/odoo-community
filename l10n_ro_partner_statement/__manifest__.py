b39fcaa13367335a42986fb2212f8d7088d537df
stananatanaela <musatnatanaela@gamil.com>
2026-06-02 13:04:35 +0300

    add l10n_ro_partner_statement, nexterp_account_allow_delete_last_invoice, nexterp_account_date_in_invoices, nexterp_account_invoice_debt_recovery, nexterp_purchase_exception

---
+++ l10n_ro_partner_statement/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2025 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Romania - Partner Statement",
+    "summary": "Romania - Partner Statement",
+    "version": "19.0.0.0.1",
+    "author": "NextERP Romania",
+    "website": "https://github.com/NextERP-Romania/odoo-community",
+    "category": "Accounting",
+    "countries": ["ro"],
+    "depends": ["partner_statement", "account"],
+    "license": "AGPL-3",
+    "data": ["views/outstanding_statement.xml", "views/statement_wizard.xml"],
+    "development_status": "Mature",
+    "maintainers": [],
+}
